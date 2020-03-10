984b69849f55cc2884882537b7418b3da5f5fa95
ez-kraivit <58212260+ez-kraivit@users.noreply.github.com>
2020-03-11 05:29:46 +0700

    Update Checkout.php

--- Checkout.php
+++ Checkout.php
@@ -3,8 +3,8 @@ echo '<pre>';
 print_r($_POST);
 echo '</pre>';
 
-define('OMISE_PUBLIC_KEY', 'pkey_test_5j654zhzz0lab3nfvk0');
-define('OMISE_SECRET_KEY', 'skey_test_56lym4o9y3ph98m4c3n');
+define('OMISE_PUBLIC_KEY', 'นำคีย์ของท่านมาใส่');
+define('OMISE_SECRET_KEY', 'นำคีย์ของท่านมาใส่');
 define('OMISE_API_VERSION', '2019-05-29');
 require dirname(__FILE__). '/omise/lib/Omise.php';
 
